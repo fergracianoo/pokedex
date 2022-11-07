46dbf2088ad592d4585045178eb900db7d0db28c
Fernando Graciano <fernandoggraciano@gmail.com>
2022-11-07 00:11:22 -0300

    fix: refactor

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from  '../../utils/api';
 import { Container, TypesWrapper, MovesWrapper } from './styles';
+import typeColors from './config';
 
 type PokemonInfo = {
   abilities: Object[],
@@ -40,26 +41,6 @@ type PokemonInfo = {
 }
 
 export default function Pokemon () {
-  const typeColors = [
-    '#beb9af',
-    '#692717',
-    '#5e76d2',
-    '#692b6b',
-    '#d1af5d',
-    '#a58d43',
-    '#8c9c0e',
-    '#5c5aab',
-    '#b2b1c1',
-    '#ea3b0b',
-    '#0d67c1',
-    '#66bb2b',
-    '#e69204',
-    '#eb447d',
-    '#6dd3f2',
-    '#735bdc',
-    '#47372a',
-    '#f0adef'
-  ];
   const params = useParams();
   const [pokemonInfo, setPokemonInfo] = useState<PokemonInfo>();
   useEffect(() => {
